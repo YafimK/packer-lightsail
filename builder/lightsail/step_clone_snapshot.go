75313ecc193b06c1e26a9a853d4b2b1f812fcc1e
YafimK <fkpkot@gmail.com>
2020-02-22 21:04:34 +0200

    parallelize builds

--- builder/lightsail/step_clone_snapshot.go
+++ builder/lightsail/step_clone_snapshot.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/lightsail"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
+	"golang.org/x/sync/errgroup"
 	"time"
 )
 
@@ -25,56 +26,63 @@ func (s *StepCloneSnapshot) Run(ctx context.Context, state multistep.StateBag) m
 		return multistep.ActionContinue
 	}
 	ui.Say(fmt.Sprintf("Deploying snapshot \"%s\" into regions: %v", *snapshot.Name, config.Regions[1:]))
-
+	errgrp, _ := errgroup.WithContext(ctx)
 	var snapshots []lightsail.InstanceSnapshot
 	for _, region := range config.Regions[1:] {
-		awsRegion := getCentralRegion(region)
-		awsCfg := &aws.Config{
-			Credentials: &creds,
-			Region:      aws.String(awsRegion),
-		}
-		newSession, err := session.NewSession(awsCfg)
-		if err != nil {
-			err := fmt.Errorf("failed setting up aws session: %v", newSession)
-			return handleError(err, state)
-		}
-		lsClient := lightsail.New(newSession)
+		region := region
+		errgrp.Go(func() error {
+			awsRegion := getCentralRegion(region)
+			awsCfg := &aws.Config{
+				Credentials: &creds,
+				Region:      aws.String(awsRegion),
+			}
+			newSession, err := session.NewSession(awsCfg)
+			if err != nil {
+				err := fmt.Errorf("failed setting up aws session: %v", newSession)
+				return err
+			}
+			lsClient := lightsail.New(newSession)
 
-		ui.Say(fmt.Sprintf("connected to AWS region -  \"%s\" ...", awsRegion))
-		ui.Say(fmt.Sprintf("creating snapshot \"%s\" in  \"%s\" ..", config.SnapshotName, config.Regions[0]))
-		_, err = lsClient.CopySnapshot(&lightsail.CopySnapshotInput{
-			SourceRegion:       snapshot.Location.RegionName,
-			SourceSnapshotName: snapshot.Name,
-			TargetSnapshotName: snapshot.Name,
-		})
-		if err != nil {
-			err = fmt.Errorf("failed cloning snapshot: %w", err)
-			return handleError(err, state)
-		}
-		ui.Say(fmt.Sprintf("waiting for snapshot \"%s\" to be ready", config.SnapshotName))
-		var snapshot *lightsail.GetInstanceSnapshotOutput
-		ticker := time.NewTicker(5 * time.Second)
-		for {
-			select {
-			case <-ticker.C:
-				snapshot, err = lsClient.GetInstanceSnapshot(&lightsail.GetInstanceSnapshotInput{InstanceSnapshotName: aws.
-					String(config.SnapshotName)})
-				if err != nil {
-					return handleError(err, state)
-				}
-				if *snapshot.InstanceSnapshot.State != lightsail.InstanceSnapshotStateAvailable {
-					continue
+			ui.Say(fmt.Sprintf("connected to AWS region -  \"%s\" ...", awsRegion))
+			ui.Say(fmt.Sprintf("creating snapshot \"%s\" in  \"%s\" ..", config.SnapshotName, region))
+			_, err = lsClient.CopySnapshot(&lightsail.CopySnapshotInput{
+				SourceRegion:       snapshot.Location.RegionName,
+				SourceSnapshotName: snapshot.Name,
+				TargetSnapshotName: snapshot.Name,
+			})
+			if err != nil {
+				err = fmt.Errorf("failed cloning snapshot: %w", err)
+				return err
+			}
+			ui.Say(fmt.Sprintf("waiting for snapshot \"%s\" in  \"%s\" ..", config.SnapshotName, region))
+			var snapshot *lightsail.GetInstanceSnapshotOutput
+			ticker := time.NewTicker(5 * time.Second)
+			for {
+				select {
+				case <-ticker.C:
+					snapshot, err = lsClient.GetInstanceSnapshot(&lightsail.GetInstanceSnapshotInput{InstanceSnapshotName: aws.
+						String(config.SnapshotName)})
+					if err != nil {
+						return err
+					}
+					if *snapshot.InstanceSnapshot.State != lightsail.InstanceSnapshotStateAvailable {
+						continue
+					}
+					break
+				case <-ctx.Done():
+					ticker.Stop()
+					return ctx.Err()
 				}
 				break
-			case <-ctx.Done():
-				ticker.Stop()
-				return handleError(ctx.Err(), state)
 			}
-			break
-		}
-		snapshots = append(snapshots, *snapshot.InstanceSnapshot)
-		ui.Say(fmt.Sprintf("Deployed snapshot \"%s\" is now in \"%s\" state", *snapshot.InstanceSnapshot.Name,
-			*snapshot.InstanceSnapshot.State))
+			snapshots = append(snapshots, *snapshot.InstanceSnapshot)
+			ui.Say(fmt.Sprintf("Deployed snapshot \"%s\" is now in \"%s\" state", *snapshot.InstanceSnapshot.Name,
+				*snapshot.InstanceSnapshot.State))
+			return nil
+		})
+	}
+	if err := errgrp.Wait(); err != nil {
+		return handleError(err, state)
 	}
 
 	state.Put("snapshots", snapshots)
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/hashicorp/packer v1.5.4
 	github.com/mitchellh/mapstructure v0.0.0-20180111000720-b4575eea38cc
 	github.com/zclconf/go-cty v1.2.1
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 )
